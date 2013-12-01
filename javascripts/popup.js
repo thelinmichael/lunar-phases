5ee23c7a30b85cfbf88d177f0c6bd3897c142a0b
Michael Thelin <thelinmichael@spotify.com>
2013-12-01 20:44:11 +0100

    Added Google Analytics

--- javascripts/popup.js
+++ javascripts/popup.js
@@ -1,6 +1,17 @@
 var req = new XMLHttpRequest();
 update();
 
+/* Google Analytics */
+var _gaq = _gaq || [];
+_gaq.push(['_setAccount', 'UA-9963785-2']);
+_gaq.push(['_trackPageview']);
+
+(function() {
+  var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+  ga.src = 'https://ssl.google-analytics.com/ga.js';
+  var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+})();
+
 function update() {
   cityCode = localStorage["chosenCityCode"];
   if (cityCode) {
